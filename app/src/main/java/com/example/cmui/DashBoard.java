03fea7f4e87557b255b3c45c0463e71f80af1775
Kamyar <k.bajan@gmail.com>
2022-12-26 19:31:02 +0100

    Added All Activities

---
+++ app/src/main/java/com/example/cmui/DashBoard.java
@@ -0,0 +1,15 @@
+package com.example.cmui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DashBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+}
